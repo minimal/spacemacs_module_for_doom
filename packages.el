727c0aff6579f52de1ccbfa872a77870001ae62d
Damon Chan <elecming@gmail.com>
2025-11-02 10:59:34 +0900

    chore(packages): conditionally load `eyebrowse` and `forge`
    Updated `packages.el` to conditionally load
    `eyebrowse` and `forge` based on the presence of
    relevant modules, improving efficiency and
    modularity.

--- packages.el
+++ packages.el
@@ -3,14 +3,16 @@
 
 (package! nov) ;; for epub layer
 (package! auto-highlight-symbol) ;; for spacemacs-navigation
-(package! eyebrowse) ;; for spacemacs-layouts
+(when (modulep! :ui workspaces)
+  (package! eyebrowse))
 (package! bm) ;; for bm layer
 (package! evil-iedit-state) ;; for spacemacs-navigation
 (package! bind-map)
 (package! symbol-overlay)
 (package! move-text) ;; for spacemacs-editing
 (package! string-inflection) ;; for spacemacs-editing
-(package! forge) ;; for github layer
+(when (modulep! :tools magit +forge)
+  (package! forge)) ;; for github layer
 (package! evil-terminal-cursor-changer) ;; for spacemacs evil
 (package! eshell-prompt-extras) ;; for shell layer
 (package! hydra) ;; for transient states
